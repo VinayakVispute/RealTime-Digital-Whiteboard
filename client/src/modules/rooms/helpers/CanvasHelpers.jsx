ee58ef84aa0e558dbb21b1a0678bfbecc2581254
NishitBaria <vinayakvispute262003@gmail.com>
2024-01-14 01:38:35 +0530

    Background Lines Added

--- client/src/modules/rooms/helpers/CanvasHelpers.jsx
+++ client/src/modules/rooms/helpers/CanvasHelpers.jsx
@@ -1,3 +1,5 @@
+import { CANVAS_SIZE } from "../../../common/constants/canvasSize";
+
 const handleMove = (move, ctx) => {
   const { options, path } = move;
   ctx.lineWidth = options.lineWidth;
@@ -11,10 +13,28 @@ const handleMove = (move, ctx) => {
   ctx.closePath();
 };
 
+export const drawBackground = (ctx) => {
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "#ccc";
+  for (let i = 0; i < CANVAS_SIZE.height; i += 25) {
+    ctx.beginPath();
+    ctx.moveTo(0, i);
+    ctx.lineTo(CANVAS_SIZE.width, i);
+    ctx.stroke();
+  }
+
+  for (let i = 0; i < CANVAS_SIZE.width; i += 25) {
+    ctx.beginPath();
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, CANVAS_SIZE.height);
+    ctx.stroke();
+  }
+};
+
 const drawAllMoves = (ctx, room) => {
   const { movesWithoutUser, usersMoves, myMoves } = room;
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
+  drawBackground(ctx);
   movesWithoutUser.forEach((move) => handleMove(move, ctx));
 
   usersMoves.forEach((userMoves) => {
